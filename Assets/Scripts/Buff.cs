8498ff558a733dc052f2ffac67e7d1646b188e42
Miguel <magudob@gmail.com>
2020-02-09 19:52:01 +0100

    09022020

---
+++ Assets/Scripts/Buff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff{
+
+    private GameObject bombPrefab;
+    private GameObject starPrefab;
+    private GameObject prefabSelected;
+
+    public Buff() {
+        bombPrefab = Resources.Load("Prefabs/Bomb") as GameObject;
+        starPrefab = Resources.Load("Prefabs/Star") as GameObject;        
+    }
+
+    public void generateBuff(GameObject brick) {
+
+        if (UnityEngine.Random.Range(0, 10) > 5) {
+            prefabSelected = bombPrefab;
+        } else {
+            prefabSelected = starPrefab;
+        }
+        GameObject powerUp = GameObject.Instantiate(prefabSelected, new Vector3(brick.transform.position.x, brick.transform.position.y, 0), Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/StarPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarPowerUp : Powers {
+    
+    private void OnCollisionEnter2D(Collision2D collision) {
+        base.OnCollisionEnter2D(collision);
+        GameObject player = collision.gameObject;
+        if (player.tag == "Player" && !player.GetComponent<PlayerController>().buffed) {
+            player.transform.localScale = new Vector3(player.transform.localScale.x*2, player.transform.localScale.y*2, player.transform.localScale.z);
+            player.GetComponent<PlayerController>().buffed = true;
+        }
+        Destroy(this.gameObject);        
+    }
+}
